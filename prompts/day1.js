4497f815dd3e3219ad064303d2484c27681c0d0d
David Leach <davidleach724@gmail.com>
2021-12-01 16:18:41 -0700

    Refactor p2 with reduce prototype

--- prompts/day1.js
+++ prompts/day1.js
@@ -1,31 +1,46 @@
-const day1data = require('../data/day1data')
+const day1data = require('../data/day1data');
 
 const findIncreaseDepth = () => {
   return day1data.depth.reduce((acc, elem, i) => {
-    if (elem < day1data.depth[i+1]) {
-      acc++
+    if (elem < day1data.depth[i + 1]) {
+      acc++;
     }
-    return acc
-  }, 0)
-}
+    return acc;
+  }, 0);
+};
 
 console.log('p1: ', findIncreaseDepth());
 
 const findIncreseWindow = () => {
-  let acum = 0
-  for( let i=0; i < day1data.depth.length; i ++) {
-    let window1 = day1data.depth[i] + 
-                  day1data.depth[i +1] + 
-                  day1data.depth[i + 2];
-    let window2 = day1data.depth[i + 1] + 
-    day1data.depth[i +2] + 
-    day1data.depth[i + 3];
+  return day1data.depth.reduce((acc, elem, i) => {
+    let window1 = elem + day1data.depth[i + 1] + day1data.depth[i + 2];
+
+    let window2 =
+      day1data.depth[i + 1] + day1data.depth[i + 2] + day1data.depth[i + 3];
 
     if (window1 < window2) {
-      acum++
+      acc++;
     }
-  }
-  return acum
-}
+    return acc;
+  }, 0);
+};
 
-console.log(findIncreseWindow());
+console.log('p2: ', findIncreseWindow());
+
+// const findIncreseWindow = () => {
+//   let acum = 0;
+//   for (let i = 0; i < day1data.depth.length; i++) {
+//     let window1 =
+//       day1data.depth[i] + day1data.depth[i + 1] + day1data.depth[i + 2];
+
+//     let window2 =
+//       day1data.depth[i + 1] + day1data.depth[i + 2] + day1data.depth[i + 3];
+
+//     if (window1 < window2) {
+//       acum++;
+//     }
+//   }
+//   return acum;
+// };
+
+
