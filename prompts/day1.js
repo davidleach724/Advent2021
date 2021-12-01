6fe5dac3e9cc0441ecdb45c3868770915a4f6dbe
David Leach <davidleach724@gmail.com>
2021-12-01 16:11:48 -0700

    Solve day1 puz 2

--- prompts/day1.js
+++ prompts/day1.js
@@ -1,13 +1,31 @@
 const day1data = require('../data/day1data')
 
-const findIncrease = () => {
-  day1data.depth.reduce((acc, elem, i) => {
+const findIncreaseDepth = () => {
+  return day1data.depth.reduce((acc, elem, i) => {
     if (elem < day1data.depth[i+1]) {
       acc++
-      console.log(acc)
     }
     return acc
   }, 0)
 }
 
-findIncrease();
+console.log('p1: ', findIncreaseDepth());
+
+const findIncreseWindow = () => {
+  let acum = 0
+  for( let i=0; i < day1data.depth.length; i ++) {
+    let window1 = day1data.depth[i] + 
+                  day1data.depth[i +1] + 
+                  day1data.depth[i + 2];
+    let window2 = day1data.depth[i + 1] + 
+    day1data.depth[i +2] + 
+    day1data.depth[i + 3];
+
+    if (window1 < window2) {
+      acum++
+    }
+  }
+  return acum
+}
+
+console.log(findIncreseWindow());
