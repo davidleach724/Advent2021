9c02becf1c5aef6f39c0215eee69e855434366c2
David Leach <davidleach724@gmail.com>
2021-12-02 08:48:40 -0700

    Add aim

--- prompts/day2.js
+++ prompts/day2.js
@@ -1,27 +1,29 @@
-const day2data = require('../data/day2data')
-const directionsData = day2data.directions
+const day2data = require('../data/day2data');
+const directionsData = day2data.directions;
 
 const determineDepth = () => {
-  let horizontal = 0
-  let depth = 0
-  
-  directionsData.forEach(direction => {
-    let value = parseInt(direction.split(' ')[1])
+  let horizontal = 0;
+  let depth = 0;
+  let aim = 0;
+
+  directionsData.forEach((direction) => {
+    let value = parseInt(direction.split(' ')[1]);
 
     if (direction.split(' ')[0] === 'forward') {
-      horizontal = horizontal + value
+      horizontal = horizontal + value;
+      depth = aim * value
     }
 
     if (direction.split(' ')[0] === 'down') {
-      depth = depth - value
+      aim = aim - value;
     }
 
     if (direction.split(' ')[0] === 'up') {
-      depth = depth + value
+      aim = aim + value;
     }
-  })
+  });
 
-  return horizontal*depth*-1
-}
+  return horizontal * depth * -1;
+};
 
 console.log(determineDepth());
