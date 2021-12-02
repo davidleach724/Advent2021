b6dc4d45e13cd93215886b2237f5f0a2199d4674
David Leach <davidleach724@gmail.com>
2021-12-02 08:36:01 -0700

    Solve day 2 puz 1

--- prompts/day2.js
+++ prompts/day2.js
@@ -1,4 +1,26 @@
 const day2data = require('../data/day2data')
 const directionsData = day2data.directions
 
-console.log(directionsData[0])
+const determineDepth = () => {
+  let horizontal = 0
+  let depth = 0
+
+  directionsData.forEach(direction => {
+
+    if (direction.split(' ')[0] === 'forward') {
+      horizontal = horizontal + parseInt(direction.split(' ')[1])
+    }
+
+    if (direction.split(' ')[0] === 'down') {
+      depth = depth - parseInt(direction.split(' ')[1])
+    }
+
+    if (direction.split(' ')[0] === 'up') {
+      depth = depth + parseInt(direction.split(' ')[1])
+    }
+  })
+
+  return horizontal*depth
+}
+
+console.log(determineDepth());
